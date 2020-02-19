71f2e59adedacebc4925529b04700d2a6fa253bb
Carolina <carolina.arcos@wolox.co>
2020-02-19 14:54:58 -0500

    finger-exercises promises and async

--- finger-exercises/ecmascript/02 - promises/index.js
+++ finger-exercises/ecmascript/02 - promises/index.js
@@ -1,9 +1,21 @@
 // Hint: use setInterval, create a new Promise and measure time with Date.now()
 
-export function delay() {
-
+export function delay(time) {
+    return new Promise((resolve, reject) => {
+        if (time < 5000) {
+            setTimeout(resolve(time + 100), 100);
+        } else {
+            reject(new Error('This time is too much !'));
+        }
+    });
 }
 
-export function asyncDelay() {
-
+export async function asyncDelay(time) {
+    return new Promise((resolve, reject) => {
+        if (time < 5000) {
+            setTimeout(resolve(time + 100), 100);
+        } else {
+            reject(new Error('This time is too much !'));
+        }
+    });
 }
