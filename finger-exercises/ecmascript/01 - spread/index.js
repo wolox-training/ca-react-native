9444e7f40088fea2cf73aef1b690ca426fbe2812
Carolina <carolina.arcos@wolox.co>
2020-02-19 12:09:12 -0500

    finger-exercise reverseMerge

--- finger-exercises/ecmascript/01 - spread/index.js
+++ finger-exercises/ecmascript/01 - spread/index.js
@@ -1,10 +1,16 @@
 import { isArray } from './utils';
 
 export function min(...value) {
-  if (!value.length) { return undefined }
+  if (!value || !value.length) { return undefined }
   return isArray(value[0]) ? Math.min(...value[0]) : Math.min(...value);
 }
 
 export function copy(value) {
   return isArray(value) ? [ ... value] : { ... value };
+}
+
+export function reverseMerge(...value) {
+  const newArray = copy(value[0]);
+  newArray.unshift(...value[1]);
+  return newArray;
 }
