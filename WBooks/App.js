845f300dbe5c02c2060d9c9e5424ee0ea87d91a4
Carolina <carolina.arcos@wolox.co>
2020-02-25 11:28:20 -0500

    Add navigation container

--- WBooks/App.js
+++ WBooks/App.js
@@ -5,23 +5,25 @@
  * @format
  * @flow
  */
-
+import 'react-native-gesture-handler';
 import React from 'react';
 import {
   SafeAreaView,
   StatusBar,
   View,
 } from 'react-native';
-
+import { NavigationContainer } from '@react-navigation/native';
 import BookList from './src/app/screens/Library/screens';
 import Header from './src/app/components/Header';
 
 const App =() => {
   return (
     <>
+    <NavigationContainer>
       <StatusBar barStyle='light-content' />
       <Header />
-        <BookList />
+      <BookList />
+    </NavigationContainer>
     </>
   );
 };
