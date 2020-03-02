b83d08951d66dae95f137733740eb3134cad408f
Carolina <carolina.arcos@wolox.co>
2020-03-02 12:26:05 -0500

    Remove commented code

--- WBooks/App.js
+++ WBooks/App.js
@@ -7,23 +7,13 @@
  */
 import 'react-native-gesture-handler';
 import React from 'react';
-// import { StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import BookList from './src/app/screens/Library/screens/BookList';
 import BookDetails from './src/app/screens/BookDetails/screens/BookDetails';
-// import Header from './src/app/components/Header';
 
 const Stack = createStackNavigator();
-
-// function LibraryView() {
-//   return (
-//       // <StatusBar barStyle='light-content' />
-//       // <Header />
-//       <BookList navigation={NavigationContainer}/>    
-//   );
-// }
 
 const App = () => (
   <>
