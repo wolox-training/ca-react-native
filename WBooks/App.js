3b1a23d6745c51566c4289453ce5158b201735ad
Carolina <carolina.arcos@wolox.co>
2020-03-06 15:20:54 -0500

    Add header to stack in tab navigator

--- WBooks/App.js
+++ WBooks/App.js
@@ -9,14 +9,13 @@ import 'react-native-gesture-handler';
 import React, { Component } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import Ionicons from 'react-native-vector-icons/Ionicons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { StatusBar, Image } from 'react-native';
 
 import BookList from './src/app/screens/Library/screens/BookList';
 import BookDetails from './src/app/screens/BookDetails/screens/BookDetails';
 import CustomHeader from './src/app/components/CustomHeader';
-import { white, blue, lightBackgroundBlue } from './src/constants/colors';
+import { white, blue } from './src/constants/colors';
 import images from './src/constants/images';
 
 const Tab = createBottomTabNavigator();
@@ -27,7 +26,7 @@ class App extends Component {
 
   customTabBarIcon = ({ icon }) => <Image source={icon} style={{ width: 25, height: 25 }} />;
 
-  LibraryStackScreen() {
+  LibraryStackScreen = () => {
     return (
       <LibraryStack.Navigator screenOptions={{ headerBackground: this.customHeader, headerTintColor: white }}>
         <LibraryStack.Screen name="Library" component={BookList} />
@@ -44,13 +43,11 @@ class App extends Component {
           <Tab.Navigator
             tabBarOptions={{
               activeTintColor: blue,
-              inactiveTintColor: 'gray',
-              showIcon: true
-            }}
-          >
+              inactiveTintColor: 'gray'
+            }}>
             <Tab.Screen
               name="Library"
-              component={BookList}
+              component={this.LibraryStackScreen}
               options={{
                 tabBarIcon: ({ focused }) => {
                   const icon = focused ? images.tabBarLibraryActive : images.tabBarLibrary;
@@ -59,10 +56,6 @@ class App extends Component {
               }}
             />
           </Tab.Navigator>
-          {/* <Stack.Navigator screenOptions={{ headerBackground: this.customHeader, headerTintColor: white }}>
-            <Stack.Screen name="Library" component={BookList} />
-            <Stack.Screen name="Details" component={BookDetails} />
-          </Stack.Navigator> */}
         </NavigationContainer>
       </>
     );
