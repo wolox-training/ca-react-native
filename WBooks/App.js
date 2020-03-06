abf910d8fe666fcf453cd945e7772f788bce4685
Carolina <carolina.arcos@wolox.co>
2020-03-06 14:25:04 -0500

    Add tabbar first element

--- WBooks/App.js
+++ WBooks/App.js
@@ -9,27 +9,60 @@ import 'react-native-gesture-handler';
 import React, { Component } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { StatusBar } from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { StatusBar, Image } from 'react-native';
 
 import BookList from './src/app/screens/Library/screens/BookList';
 import BookDetails from './src/app/screens/BookDetails/screens/BookDetails';
 import CustomHeader from './src/app/components/CustomHeader';
-import { white } from './src/constants/colors';
+import { white, blue, lightBackgroundBlue } from './src/constants/colors';
+import images from './src/constants/images';
 
-const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+const LibraryStack = createStackNavigator();
 
 class App extends Component {
   customHeader = () => <CustomHeader />;
+
+  customTabBarIcon = ({ icon }) => <Image source={icon} style={{ width: 25, height: 25 }} />;
+
+  LibraryStackScreen() {
+    return (
+      <LibraryStack.Navigator screenOptions={{ headerBackground: this.customHeader, headerTintColor: white }}>
+        <LibraryStack.Screen name="Library" component={BookList} />
+        <LibraryStack.Screen name="Details" component={BookDetails} />
+      </LibraryStack.Navigator>
+    );
+  }
 
   render() {
     return (
       <>
         <NavigationContainer>
           <StatusBar barStyle="light-content" />
-          <Stack.Navigator screenOptions={{ headerBackground: this.customHeader, headerTintColor: white }}>
+          <Tab.Navigator
+            tabBarOptions={{
+              activeTintColor: blue,
+              inactiveTintColor: 'gray',
+              showIcon: true
+            }}
+          >
+            <Tab.Screen
+              name="Library"
+              component={BookList}
+              options={{
+                tabBarIcon: ({ focused }) => {
+                  const icon = focused ? images.tabBarLibraryActive : images.tabBarLibrary;
+                  return <this.customTabBarIcon icon={icon} />;
+                }
+              }}
+            />
+          </Tab.Navigator>
+          {/* <Stack.Navigator screenOptions={{ headerBackground: this.customHeader, headerTintColor: white }}>
             <Stack.Screen name="Library" component={BookList} />
             <Stack.Screen name="Details" component={BookDetails} />
-          </Stack.Navigator>
+          </Stack.Navigator> */}
         </NavigationContainer>
       </>
     );
--- WBooks/src/constants/images.js
+++ WBooks/src/constants/images.js
@@ -1,5 +1,7 @@
 import defaultBookImage from '../app/assets/default_book_image.png';
 import defaultProfileImage from '../app/assets/profile_picture.png';
 import navBarImage from '../app/assets/bc_nav_bar.png';
+import tabBarLibrary from '../app/assets/ic_library.png';
+import tabBarLibraryActive from '../app/assets/ic_library_active.png';
 
-export default { defaultBookImage, defaultProfileImage, navBarImage };
+export default { defaultBookImage, defaultProfileImage, navBarImage, tabBarLibrary, tabBarLibraryActive };
